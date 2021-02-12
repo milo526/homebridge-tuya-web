dc89a94d27bbafd98213a3be33570fe03c4303f3
Alex Pinhasov <alex5872205@gmail.com>
2021-02-12 22:08:55 +0200

    Run prettier and linter, add #149 issue code

--- src/accessories/characteristics/brightness.ts
+++ src/accessories/characteristics/brightness.ts
@@ -63,7 +63,9 @@ export class BrightnessCharacteristic extends TuyaWebCharacteristic {
     ) {
       stateValue = Number(data.color.brightness);
     } else if (data?.brightness && data?.max_brightness) {
-        stateValue = Math.round((Number(data.brightness) / data.max_brightness) * 100);
+      stateValue = Math.round(
+        (Number(data.brightness) / data.max_brightness) * 100
+      );
     }
 
     if (stateValue) {
